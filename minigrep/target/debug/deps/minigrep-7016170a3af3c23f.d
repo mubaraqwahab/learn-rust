7a7437324c1d46c9fd95d2c4225755100b118202
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-10-25 19:32:09 +0100

    init minigrep

---
+++ minigrep/target/debug/deps/minigrep-7016170a3af3c23f.d
@@ -0,0 +1,5 @@
+/home/mubwa/dev/learn_rust/minigrep/target/debug/deps/libminigrep-7016170a3af3c23f.rmeta: src/main.rs
+
+/home/mubwa/dev/learn_rust/minigrep/target/debug/deps/minigrep-7016170a3af3c23f.d: src/main.rs
+
+src/main.rs:
