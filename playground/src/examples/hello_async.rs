6ad0fbe8a690c33e314b0dc35de1eacc2f15f889
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-10-30 13:30:51 +0100

    ...

--- playground/src/examples/hello_async.rs
+++ playground/src/examples/hello_async.rs
@@ -1,9 +1,10 @@
 use trpl::Html;
 
-pub async fn page_title(url: &str) -> Option<String> {
+pub async fn page_title(url: &str) -> (&str, Option<String>) {
     let response = trpl::get(url).await;
     let response_text = response.text().await;
-    Html::parse(&response_text)
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title_element| title_element.inner_html())
+        .map(|title_element| title_element.inner_html());
+    (url, title)
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,15 +3,24 @@
 mod examples;
 
 use examples::hello_async::*;
+use trpl::Either;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let fut1 = page_title(&args[1]);
+        let fut2 = page_title(&args[2]);
+
+        let (url, title) = match trpl::race(fut1, fut2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
         };
+
+        println!("{url} returned first");
+        match title {
+            Some(title) => println!("Its page title is: {title}"),
+            None => println!("Its page title couldn't be parsed"),
+        }
     });
 }
