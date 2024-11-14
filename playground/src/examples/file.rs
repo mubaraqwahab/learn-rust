2204d7f81fccbd53286c214d44ea6bbe43406f57
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-11-14 13:30:58 +0100

    create new webserver package

--- playground/src/examples/file.rs
+++ playground/src/examples/file.rs
@@ -1,13 +1,17 @@
 use std::{
     fs::File,
-    io::{Read, Seek},
+    io::{Read, Seek, Write},
 };
 
 pub fn file_io() {
     let mut f = File::open("./README.md").unwrap();
-    f.seek(std::io::SeekFrom::Start(10)).unwrap();
+
+    let initial_pos = f.stream_position().unwrap();
+    println!("initial pos: {}", initial_pos);
+
     let mut buf = [0u8; 10];
     f.read(&mut buf).unwrap();
-    let s: String = buf.iter().map(|&n| char::from(n)).collect();
+
+    let s = String::from_utf8(buf.into()).unwrap();
     println!("{:?}", s);
 }
