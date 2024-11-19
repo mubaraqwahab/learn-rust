1f0824907043492daf41831855914e5d34a62882
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-11-19 22:57:49 +0100

    very basic grrs

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to file to search
+    path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let file = File::open(&args.path)
+        .with_context(|| format!("could not open file `{}`", args.path.display()))?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
